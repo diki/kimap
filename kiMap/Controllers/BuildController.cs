c9273f83d7ce1c5c33cc4066ca3a32137eeeed76
mdikici <musdikici@gmail.com>
2012-06-23 04:42:08 +0300

    page build controller and toolbox to add new widgets

---
+++ kiMap/Controllers/BuildController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kiMap.Controllers
+{
+    public class BuildController : Controller
+    {
+        //
+        // GET: /Build/
+
+        
+        public JsonResult Page(string s, string t)
+        {
+           // return "ooooo";
+
+            //return default models for collection if source is new
+            var DefaultHeaderModel = new
+            {
+                type = "header",
+                header = "Mesajınızı buraya yazın",
+                subheader = "Alt başlık ekleyin",
+                order = 0
+            };
+
+            var DefaultTextModel = new
+            {
+                type= "text",
+                title= "Bu işte en iyi biziz",
+                content= "Ne iş yapıyorsunuz. Sizin için önemi nedir. Ürün, servis ...",
+                order = 1
+            };
+
+            //title
+            var DefaultTitleModel = new {
+                type = "title",
+                content = "Ürünlerimiz",
+                order = 3
+            };
+
+            //gallery
+            var DefaultGalleryModel = new {
+                type =  "gallery",
+                title1 =  "title 1",
+                content1 =  "content 1",
+                title2 =  "title 2",
+                content2 =  "content 2",
+                title3 =  "title 3",
+                content3 =  "content 3",
+                order = 4
+                
+            };
+
+            //bio
+            var DefaultBioModel = new {
+                type =  "bio",
+                title =  "Biz !!!",
+                address =  "Adrs",
+                phone =  "phone",
+                email =  "email",
+                website =  "website",
+                order = 5
+            };
+
+            List<Object> models = new List<object>();
+            models.Add(DefaultHeaderModel);
+            models.Add(DefaultTextModel);
+            models.Add(DefaultTitleModel);
+            models.Add(DefaultGalleryModel);
+            models.Add(DefaultBioModel);
+
+            return Json(new { data=models, success = true, source = s, type=t }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ kiMap/Scripts/views/WidgetBoxView.js
@@ -0,0 +1,63 @@
+﻿/* File Created: Haziran 18, 2012 */
+
+var WidgetBoxView = Backbone.View.extend({
+
+    el: "#widgets-gallery-box",
+
+    events: {
+        "click li": "addNewModelFromWidgetBox"
+    },
+    initialize: function () {
+
+        _.bindAll(this, "render", "appendWidget", "addNewModelFromWidgetBox");
+
+        //this.render();
+
+
+
+    },
+    render: function () {
+
+        this.collection.each(this.appendWidget);
+        return this;
+    },
+
+    appendWidget: function (widgetModel) {
+        var v = new WidgetView({ model: widgetModel });
+
+        $(this.el).append(v.el);
+    },
+
+    addNewModelFromWidgetBox: function (e) {
+        var widgetType = $(e.target).attr("id");
+
+        var el = $(e.target).parents("ul");
+
+        if (!editorView.selectedWidget) {
+            return;
+        }
+
+        var vidgetTemplate = $("#" + widgetType + "-widget-template");
+        //TODO: move this method to a mamager object
+        switch (widgetType) {
+            case "title":
+                var v = new WidgetView({ model: DefaultTitleModel });
+                $(editorView.selectedWidget.el).after(v.el);
+                break;
+            case "text":
+                var v = new WidgetView({ model: DefaultTextModel });
+                $(editorView.selectedWidget.el).after(v.el);
+                break;
+            case "gallery":
+                break;
+            case "bio":
+                break;
+            case "close":
+                console.log("close");
+                el.hide();
+            default:
+        }
+    }
+
+
+});
